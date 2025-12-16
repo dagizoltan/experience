d5d3371f458516c73d56384fad7726991052a500
dagizoltan <dagizoltan@gmail.com>
2025-12-16 18:43:29 +0100

    db path

--- src/adapters/kv/index.js
+++ src/adapters/kv/index.js
@@ -4,5 +4,5 @@
 export const createKv = async (path) => {
   // Ensure we share the same DB file locally
   const dbPath = path || './data.db';
-  return await Deno.openKv(dbPath);
+  return await Deno.openKv();
 }
